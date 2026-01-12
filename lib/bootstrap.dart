8ad694289f8f5208ba3048fcfd2cc27d83a3547c
limcheekin <limcheekin@vobject.com>
2026-01-12 17:04:27 +0800

    fix: resolve linux build/runtime errors and unify app initialization

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,14 +1,23 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:ffi';
 
 import 'package:flutter/widgets.dart';
+import 'package:offline_sync/app/app.locator.dart';
+import 'package:sqlite3/open.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  // Add cross-flavor configuration here
+  open.overrideFor(OperatingSystem.linux, () {
+    return DynamicLibrary.open('libsqlite3.so.0');
+  });
+
+  await setupLocator();
 
   runApp(await builder());
 }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:offline_sync/app/app.locator.dart';
 import 'package:offline_sync/app/main_app.dart';
+import 'package:offline_sync/bootstrap.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setupLocator();
-  runApp(const MainApp());
+  await bootstrap(() => const MainApp());
 }
